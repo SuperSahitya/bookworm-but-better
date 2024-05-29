f0e15b9bbd3b833ac9838fde582a253d558f31b2
Sahitya Kumar Choubey <sahityakumarchoubey@gmail.com>
2024-05-30 00:05:56 +0530

    fix order route

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,5 +1,6 @@
-import type { NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   console.log(request);
+  return NextResponse.json({ test: "true" });
 }
